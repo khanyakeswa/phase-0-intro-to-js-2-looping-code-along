c114d8da482bb07fb03a33008370ea6d3ce93a21
Khanya Keswa <khanya.keswa@gmail.com>
2022-10-02 14:46:40 -0500

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names, gift) {
+    let newArray = [];
+    for (let i = 0; i < 3; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${gift} gift!`);
+    }
+    return newArray;
+}
+
+function countDown(n) {
+    for (let i = n; i >= 0; i--) {
+        console.log(i);
+    }
+}
